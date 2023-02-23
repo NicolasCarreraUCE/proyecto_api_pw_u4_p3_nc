5da720b3923cb7a78de43f5f0c669692f08f25e4
GeoWillC <nacarrera@uce.edu.ec>
2023-02-22 19:48:21 -0500

    taller 33

---
+++ src/main/java/com/example/proyecto_api_pw_u4_p3_nc/ProyectoApiPwU4P3NcApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_pw_u4_p3_nc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiPwU4P3NcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiPwU4P3NcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_api_pw_u4_p3_nc/ProyectoApiPwU4P3NcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_pw_u4_p3_nc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiPwU4P3NcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
